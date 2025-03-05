b46debe033647fcf04533d69bb669335e49bda89
Abdul Basit <ab1438068@gmail.com>
2025-03-05 20:51:13 +0500

    new file:   cache-logics/src/assets/arrow-up-right.svg 	new file:   cache-logics/src/assets/social-icon-5.svg 	new file:   cache-logics/src/assets/white-icon-1.svg 	new file:   cache-logics/src/assets/white-icon-2.svg 	modified:   cache-logics/src/css/Footer.css 	modified:   cache-logics/src/css/Team.css 	modified:   cache-logics/src/pages/Team.jsx

--- cache-logics/src/pages/Team.jsx
+++ cache-logics/src/pages/Team.jsx
@@ -5,6 +5,11 @@ import "../css/Team.css";
 import ceo from "../assets/ceo.png";
 import member from "../assets/member-2.png";
 import member2 from '../assets/member-3.png';
+import arrow from "../assets/arrow-up-right.svg";
+import socialIcon1 from "../assets/white-icon-1.svg";
+import socialIcon2 from "../assets/white-icon-2.svg";
+import socialIcon3 from "../assets/social-icon-5.svg";
+
 
 const Team = ()=>{
     return (
@@ -19,21 +24,33 @@ const Team = ()=>{
 
         <div className="team-container">
              <div className="image-1">
-                <img src={ceo} alt="" />
+                <img className="ceo" src={ceo} alt="" />
+                <img className="ceo-arrow-icon" src={arrow} alt=""/>  
                 <div className="content-1 first">
                 <h3>Faizan Ahmed</h3>
                 <p id="founder">Founder & CEO</p>
                 <p>Former co-founder of Opendoor. Early staff at Apple and Google.</p>
                 </div>
+                <div className="social-media">
+                <img src={socialIcon3} alt="Social Icon 3" />
+                <img src={socialIcon1} alt="Social Icon 1" />
+                <img src={socialIcon2} alt="Social Icon 2" />
+          </div>
                 </div>
 
-                <div className="image-2">
-                <img src={member} alt="" />
+                <div className="image-2"> 
+                <img className="co-founder" src={member} alt="" />
+                <div className="content-1 second">
+                <img className="arrow-icon" src={arrow} alt="" />
+                    <h3>Abdullah Sarfaraz</h3>
+                    <p>Co-Founder</p>
+                </div>   
                 </div>
 
                 <div className="image-3">
-                <img src={member2} alt="" />
+                <img className="product-manager" src={member2} alt="" />
                 <div className="content-1 third">
+                <img className="arrow-icon" src={arrow} alt="" />
                     <h3>Annie Stanley</h3>
                     <p>Product Manager</p>
                 </div>
